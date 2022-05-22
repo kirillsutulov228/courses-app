eadc88fbcc49d5aaeec68b4fb33c2788ff3ffa49
Sutulov Kirill <kirillsutulov228@mail.ru>
2022-05-22 23:06:38 +0400

    added all courses page

---
+++ frontend/src/pages/AllCourses/AllCourses.js
@@ -0,0 +1,17 @@
+import Courses from '../../components/Courses/Courses.js';
+import api from './../../axios';
+import './AllCourses.css';
+
+export default function AllCourses(props) {
+  async function getAllCourses({ page, setMaxPage, limit }) {
+    const result = await api.get('/courses', { params: { page, limit } });
+    setMaxPage(Math.ceil(result.data.total / limit));
+    return result.data.result;
+  }
+
+  return (
+    <div className='all-courses'>
+      <Courses title={"Список курсов"} style={{flex: '1'}} fetchCourses={getAllCourses} limit={10} />
+    </div>
+  );
+}
