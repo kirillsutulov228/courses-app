aad35620e7050b2c0e3d0c4a755a2fe660414548
Sutulov Kirill <kirillsutulov228@mail.ru>
2022-05-24 00:38:04 +0400

    added main page

---
+++ frontend/src/pages/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import './MainPage.css';
+import logo from '../../assets/logo.svg';
+
+export default function MainPage() {
+  return (
+    <div className='main-page'>
+      <img src={logo} alt='' />
+      <h1>Добро пожаловать в Moodlo</h1>
+      <h2>Здесь каждый может создать курс, который смогут пройти 100500 людей по всему миру!</h2>
+    </div>
+  );
+}
